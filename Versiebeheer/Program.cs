80e4a13cd8371d1714cd2283ff2fb054e789b338
Gianni Debruyne <debruyne.gianni@hotmail.com>
2017-05-05 14:30:46 +0200

    eerste commit

--- Versiebeheer/Program.cs
+++ Versiebeheer/Program.cs
@@ -10,6 +10,8 @@ namespace Versiebeheer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
